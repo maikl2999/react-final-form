16ebc7c8557d62030d05dd56d04f50d78878e0be
maikl2999 <maikl2929@gmail.com>
2019-10-27 18:37:13 +0200

    first upload

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+
+class App extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <Form />
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
